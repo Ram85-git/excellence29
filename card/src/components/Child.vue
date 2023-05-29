96cbd0d3ff90535e4ded792ac79181c270bb2a40
Ram <126403725+Ram85-git@users.noreply.github.com>
2023-05-29 13:36:12 -0400

    img added

---
+++ card/src/components/Child.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="cart">
+        
+      <header>
+       <slot name="header"></slot>
+      </header>
+    
+       <section>
+        <slot name="title"></slot>
+       </section>
+       
+      <content>
+       <slot name="main"></slot>
+      </content>
+      <footer>
+        <slot name="footer"></slot>
+      </footer>
+    
+    </div>
+    
+    </template>
+    
+    
+    <script>
+    
+    export default {
+        name:`Child`,
+    
+    
+    
+    }
+    
+    </script>
+    <style scoped>
+    .cart{
+        border: 2px solid green;
+        width: 400px;
+        height: 500px;
+        margin: auto;
+    
+    }
+    
+    </style>
