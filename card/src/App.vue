7efcbf84e90f2660add9c5d6d6913bdef013a85a
Ram <126403725+Ram85-git@users.noreply.github.com>
2023-05-29 13:35:40 -0400

    Card added

--- card/src/App.vue
+++ card/src/App.vue
@@ -1,16 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+  
+  <Home/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue';
+
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: `App`,
+    components: {
+      Home
+      
+    }
+    
 }
 </script>
 
